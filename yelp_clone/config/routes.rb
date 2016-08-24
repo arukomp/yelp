02315a221e4c45342ec68fe9f73e992a77dc2c51
Frunez <francesco.nunez@gmail.com>
2016-08-24 15:06:07 +0100

    added facebook login option ch4 v2

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root "restaurants#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-root "restaurants#index"
 
 get 'restaurants' => 'restaurants#index'
   resources :restaurants do
