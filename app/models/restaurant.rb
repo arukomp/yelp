200939eb5586fd27cd85d137ef15a631f9a58106
Arunas Skirius <arukomp@gmail.com>
2016-08-26 09:56:57 +0100

    fixed v3 section 3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,14 @@
 class Restaurant < ActiveRecord::Base
   # belongs_to :user
-  has_many :reviews, -> { extending WithUserAssociationExtension },
-  dependent: :destroy
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  def build_review(attributes={}, user)
-    review = reviews.build(review_params)
-    review.user = current_user
-    review
-  end
+  # def build_review(review_params={}, user)
+  #   review = reviews.build(review_params)
+  #   review.user = user
+  #   review
+  # end
 
 end
